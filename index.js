f71fc5c4a25e8c1aaa625e6b1ddeff0de79aea0e
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-07-26 18:23:45 -0700

    express routes

--- index.js
+++ index.js
@@ -19,9 +19,16 @@ let notes = [
     important: true
   }
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(notes));
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;
